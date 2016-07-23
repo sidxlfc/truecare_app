98c80cc266985f18618176132200e6144ddfe199
sidxlfc@gmail.com <sid.xlfc@gmail.com>
2016-07-23 16:12:38 -0500

    Add project files.

---
+++ truecare_app/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace truecare_app
+{
+    [Activity(Label = "truecare_app", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private List<string> mItems;
+
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            //initialization
+            mListView = FindViewById<ListView>(Resource.Id.myListView);
+
+            ArrayAdapter<string> adapter;
+
+            //main logic
+
+            mItems = new List<string>();
+            mItems.Add("Bob");
+            mItems.Add("Tom");
+            mItems.Add("Jim");
+
+            adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            mListView.Adapter = adapter;
+       }
+    }
+}
+
