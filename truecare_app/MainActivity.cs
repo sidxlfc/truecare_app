514dbf0aec4345250f100fd396e5e5d7beb8b7fa
sidxlfc@gmail.com <sid.xlfc@gmail.com>
2016-07-23 19:42:22 -0500

    XML Data Parsed :)

--- truecare_app/MainActivity.cs
+++ truecare_app/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Xml;
 
 namespace truecare_app
 {
@@ -15,6 +18,8 @@ namespace truecare_app
         private List<string> mItems;
 
         private ListView mListView;
+
+        ISharedPreferences isp;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -28,12 +33,56 @@ namespace truecare_app
 
             ArrayAdapter<string> adapter;
 
-            //main logic
+            //making the connection and defining request properties
+            var rxcui = "198440";
+            var request = HttpWebRequest.Create(string.Format(@"http://rxnav.nlm.nih.gov/REST/RxTerms/rxcui/{0}/allinfo", rxcui));
+            request.ContentType = "application/json";
+            request.Method = "GET";
+
+            //sending the request
+            var content = "";
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                    Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    content = reader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(content))
+                    {
+                        Console.Out.WriteLine("Response contained empty body...");
+                    }
+                    else
+                    {
+                        Console.Out.WriteLine("Response Body: \r\n {0}", content);
+                    }
+
+                    //Assert.NotNull(content);
+                }
+            }
+
 
             mItems = new List<string>();
-            mItems.Add("Bob");
-            mItems.Add("Tom");
-            mItems.Add("Jim");
+
+            //process the XML string
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(content);
+
+            XmlNodeList displayName = xmlDoc.GetElementsByTagName("displayName");
+            XmlNodeList synonym = xmlDoc.GetElementsByTagName("synonym");
+            XmlNodeList fullName = xmlDoc.GetElementsByTagName("fullName");
+            XmlNodeList strength = xmlDoc.GetElementsByTagName("strength");
+            XmlNodeList route = xmlDoc.GetElementsByTagName("route");
+
+
+            //main logic
+
+            mItems.Add("Name of the medicine : " + displayName[0].InnerText);
+            mItems.Add("Synonym : " + synonym[0].InnerText);
+            mItems.Add("fullName : " + fullName[0].InnerText);
+            mItems.Add("Strength : " + strength[0].InnerText);
+            mItems.Add("Route : " + route[0].InnerText);
 
             adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
 
